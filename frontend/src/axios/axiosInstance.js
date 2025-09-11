96f958d7aada94385315696b7e1117bb9bb751c2
Raushankumar4 <raushankumarguptag@gmail.com>
2025-09-11 15:45:56 +0530

    Frontend Setup with axios tanstack query react-hook-form tailwind css

---
+++ frontend/src/axios/axiosInstance.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_URI}/api/v1`,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance
