f3d70f5889cfe29218e6f33ed26ddd436f001270
Raushankumar4 <raushankumarguptag@gmail.com>
2025-09-11 16:04:23 +0530

    added AuthContext for user authentication state management

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(() => localStorage.getItem("token"));
+  const [user, setUser] = useState(() => {
+    const userData = localStorage.getItem("user");
+    return userData ? JSON.parse(userData) : null;
+  });
+
+  const login = ({ token, user }) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
+    setToken(token);
+    setUser(user);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setToken(null);
+    setUser(null);
+  };
+  return (
+    <AuthContext.Provider
+      value={{ token, user, login, logout, isAuthenticated: !!token }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
